5bc753ee53c3a962835582fcc2dcb47129273ee8
reddypavithra21 <reddypavibathala02@gmail.com>
2026-06-03 13:12:03 +0530

    added development file

--- Devolpment.sh
+++ Devolpment.sh
@@ -1,3 +1,4 @@
+#!/bin/install/
 cd /opt
 sudo yum install wget -y
 # In case of Ubuntu/Debiain
@@ -6,7 +7,6 @@ sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.115/bin/apache-tomcat-9.
 sudo tar -xvzf apache-tomcat-9.0.115.tar.gz
 sudo mv apache-tomcat-9.0.115 tomcat
 sudo rm apache-tomcat-9.0.115.tar.gz
-
 # Set permissions
 sudo chown -R ec2-user:ec2-user /opt/tomcat
 # In Case Of Ubuntu
